4dcb4640bf66394f5b8ec3b43d8e9090dac34760
mauricelehmann <m.lehmann94@gmail.com>
2019-01-08 20:58:33 +0100

    t

--- recherche.cpp
+++ recherche.cpp
@@ -3,5 +3,6 @@
 using namespace std;
 
 strIterator rechercheDichotomique(const strVector& dico,const string& mot);
+
 //Surcharge de la premier fonction -> TODO : Doc doxygen
 strIterator rechercheDichotomique(const strIterator& itBegin,const strIterator& itEnd,const string& mot);
