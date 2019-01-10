f74589a36a1bdea53f027d8cd6fc135a600196cd
gab-thr <gabrielle.thurnherr@gmail.com>
2019-01-10 11:28:03 +0100

    add fonction inverser

--- main.cpp
+++ main.cpp
@@ -9,6 +9,18 @@ int main(){
     vector<string> dico;
     dico = lecture("dico/nato.txt");
 
+    //test inverser
+    // for(auto mot : dico) {
+    //   cout << mot << endl;
+    // }
+    //
+    // cout << endl << endl;
+    //
+    // inverser(dico);
+    //
+    // for(auto mot : dico) {
+    //   cout << mot << endl;
+    // }
 
     return EXIT_SUCCESS;
 }
